f68f4f4d732f6e3d278750adcb09b21889e3d769
ivan <ivansekulic4@gmail.com>
2020-02-26 14:47:55 +0100

    dodato

---
+++ WinterWorkShop.Cinema.API/Models/TagModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WinterWorkShop.Cinema.API.Models
+{
+    public class TagModel
+    {
+        
+        public int Id { get; set; }
+
+       
+        public string Value { get; set; }
+    }
+}
