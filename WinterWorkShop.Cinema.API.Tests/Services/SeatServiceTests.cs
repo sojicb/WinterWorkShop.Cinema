76df3f3a945f0c10261a440813d32aef547ba26b
bogoljub.sojic987 <bogoljub.sojic987@gmail.com>
2020-03-11 16:36:41 +0100

    Service Tests: Covered SeatService

--- WinterWorkShop.Cinema.API.Tests/Services/SeatServiceTests.cs
+++ WinterWorkShop.Cinema.API.Tests/Services/SeatServiceTests.cs
@@ -2,18 +2,20 @@
 using Moq;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WinterWorkShop.Cinema.Data;
 using WinterWorkShop.Cinema.Domain.Models;
 using WinterWorkShop.Cinema.Domain.Services;
+using WinterWorkShop.Cinema.Repositories;
 
 namespace WinterWorkShop.Cinema.Tests.Services
 {
     [TestClass]
     public class SeatServiceTests
     {
-        private Mock<ISeatRepository> _mockSeatsRepository;
+        private Mock<ISeatsRepository> _mockSeatsRepository = new Mock<ISeatsRepository>();
         private Seat _seat;
         private SeatDomainModel _seatDomainModel;
 
@@ -36,17 +38,55 @@ namespace WinterWorkShop.Cinema.Tests.Services
                 Row = 1,
                 Number = 1
             };
-
-            List<Seat> seatsModelList = new List<Seat>();
             
+        }
+
+        [TestMethod]
+        public void SeatService_GetAllAsync_ReturnSeats()
+        {
+            //Arrange
+            int expectedResult = 1;
+            List<Seat> seatsModelList = new List<Seat>();
             seatsModelList.Add(_seat);
             IEnumerable<Seat> seats = seatsModelList;
             Task<IEnumerable<Seat>> responseTask = Task.FromResult(seats);
 
-            _mockSeatsRepository = new Mock<ISeatRepository>();
             _mockSeatsRepository.Setup(x => x.GetAll()).Returns(responseTask);
+
+            SeatService seatService = new SeatService(_mockSeatsRepository.Object);
+
+            //Act
+            var resultAction = seatService.GetAllAsync()
+                .ConfigureAwait(false)
+                .GetAwaiter()
+                .GetResult()
+                .ToList();
+
+            //Assert
+            Assert.IsNotNull(resultAction);
+            Assert.AreEqual(expectedResult, resultAction.Count);
         }
 
-       
+        [TestMethod]
+        public void SeatService_GetAllAsync_InsertNull_ReturnNull()
+        {
+            //Arrange
+            IEnumerable<Seat> seats = null;
+            Task<IEnumerable<Seat>> responseTask = Task.FromResult(seats);
+
+            _mockSeatsRepository.Setup(x => x.GetAll()).Returns(responseTask);
+
+            SeatService seatService = new SeatService(_mockSeatsRepository.Object);
+
+            //Act
+            var resultAction = seatService.GetAllAsync()
+                .ConfigureAwait(false)
+                .GetAwaiter()
+                .GetResult();
+
+            //Assert
+            Assert.IsNull(resultAction);
+        }
+
     }
 }
